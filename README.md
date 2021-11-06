97cf6ae7bfee6f82b921579a590809e5dd4be71e
teacherMaJ <1506889809@qq.com>
2021-11-06 10:29:48 +0800

    组织架构

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
        1. 此时可以通过props来进行设置，props是一个对象，对象内有两个属性值 label，children
        2. 其中label可以指定你要用什么属性作标题；children可以让你设置用什么属性值作子节点
     3. props的作用就是告诉el-tree去哪里找label内容，去哪里找子节点
+## el-row
+   1. 在el-row布局中,如果用align来设置元素的位置，那么需要给el-row一个定高
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,19 +1,79 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <el-card class="tree-card">
+         <!-- 头部 -->
+         <el-row type="flex" justify="space-between" style="height:40px" align="bottom">
+           <el-col>
+              <span>组织架构</span>
+           </el-col>
+           <el-col :span="6">
+               <el-row type="flex" justify="end">
+                  <el-col>负责人</el-col>
+                  <el-col>
+                    <el-dropdown>
+                        <span>操作<i class="el-icon-arrow-down el-icon--right"></i></span>
+                         <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item>添加子部门</el-dropdown-item>
+                            <!-- <el-dropdown-item>编辑部门</el-dropdown-item>
+                            <el-dropdown-item>删除部门</el-dropdown-item>  -->
+                         </el-dropdown-menu>
+                    </el-dropdown>
+                  </el-col>
+               </el-row>
+           </el-col>
+         </el-row>
+         <!-- 树形结构 -->
+         <el-tree
+           :data="departs"
+           :props="defaultProps"
+         >
+             <template slot-scope="{data}">
+                    <el-row type="flex" justify="space-between" style="height:40px;width:100%" align="middle">
+                      <el-col>
+                          <span>{{data.name}}</span>
+                      </el-col>
+                      <el-col :span="6">
+                          <el-row type="flex" justify="end">
+                              <el-col>负责人</el-col>
+                              <el-col>
+                                <el-dropdown>
+                                    <span>操作<i class="el-icon-arrow-down el-icon--right"></i></span>
+                                    <el-dropdown-menu slot="dropdown">
+                                        <el-dropdown-item>添加子部门</el-dropdown-item>
+                                        <el-dropdown-item>编辑部门</el-dropdown-item>
+                                        <el-dropdown-item>删除部门</el-dropdown-item> 
+                                    </el-dropdown-menu>
+                                </el-dropdown>
+                              </el-col>
+                          </el-row>
+                      </el-col>
+                    </el-row>
+             </template>
+         </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name:"departments",
+   data() {
+    return {
+      defaultProps: {
+        label: 'name'
+      },
+      departs: [{ name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' }, { name: '人事部' }]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .tree-card {
+    padding: 30px  140px;
+    font-size:14px;
+  }
 </style>
